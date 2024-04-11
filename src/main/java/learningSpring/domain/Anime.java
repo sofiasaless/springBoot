a28084142bbaa786f850461395254aaacbe0f49d
sofiasales02 <sofiasaleswk@gmail.com>
2024-04-11 11:32:54 -0300

    data jpa pt.1

--- src/main/java/learningSpring/domain/Anime.java
+++ src/main/java/learningSpring/domain/Anime.java
@@ -1,11 +1,22 @@
 package learningSpring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+// é preciso ter um construtor sem argumentos depois de colocar o entity
+@NoArgsConstructor
+// transformando o anime em entidade
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
