ae9bc92331a5d7b2b586a11790515f1e7e1d3261
sofiasales02 <sofiasaleswk@gmail.com>
2024-05-31 15:56:05 -0300

    (NEW BRNCH) spring security: autenticação em memoria

--- src/main/java/learningSpring/config/SecurityConfig.java
+++ src/main/java/learningSpring/config/SecurityConfig.java
@@ -1,43 +1,3 @@
-//package learningSpring.config;
-//
-//import lombok.extern.log4j.Log4j2;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-////@EnableWebSecurity
-//@Log4j2
-//public class SecurityConfig extends WebSecurityConfiguration {
-//
-////    protected void configure(HttpSecurity http) throws Exception {
-////        http.authorizeRequests()
-////                .anyRequest()
-////                .authenticated()
-////                .and()
-////                .httpBasic();
-////    }
-////
-////    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-////        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-////        log.info("Password encoded {}", passwordEncoder.encode("test"));
-////    }
-//
-//
-//
-//}
-
 package learningSpring.config;
 
 import lombok.extern.log4j.Log4j2;
@@ -57,7 +17,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Log4j2
 public class SecurityConfig {
 
-    // protegendo todas as requisições http
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
@@ -70,10 +29,36 @@ public class SecurityConfig {
     public InMemoryUserDetailsManager userDetailsService() {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         UserDetails user = User.withUsername("sofia")
-                .password(encoder.encode("balda"))
-                .roles("USER")
+                .password(encoder.encode("test"))
+                .roles("USER", "ADMIN")
                 .build();
-        log.info("Password encoded {} ", encoder.encode("balda"));
         return new InMemoryUserDetailsManager(user);
     }
+
+
+// OS METODOS ABAIXO ESTÃO DEPRECIADOS
+
+
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//        .httpBasic();
+//    }
+//
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        log.info("Password encoded {}", passwordEncoder.encode("test"));
+//
+//        auth.inMemoryAuthentication()
+//            .withUser("sofia")
+//                .password(passwordEncoder.encode("test"))
+//                .roles("ADMIN", "USER")
+//                .and()
+//                .withUser("usuario")
+//                .password(passwordEncoder.encode("test"))
+//        .roles("USER");
+//    }
+
 }
