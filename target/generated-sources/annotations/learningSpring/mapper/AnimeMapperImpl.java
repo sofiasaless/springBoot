209ddff2bc31bb41ee1aa6b6dc0fb811fbb42e66
sofiasales02 <sofiasaleswk@gmail.com>
2024-06-03 21:32:46 -0300

    documentação com openapi pt.1 - dependências

--- target/generated-sources/annotations/learningSpring/mapper/AnimeMapperImpl.java
+++ target/generated-sources/annotations/learningSpring/mapper/AnimeMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-03T16:40:59-0300",
+    date = "2024-06-03T21:26:27-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 18.0.2-ea (Private Build)"
 )
 @Component
